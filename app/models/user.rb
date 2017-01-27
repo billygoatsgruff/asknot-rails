0e5d55069a7e32a9b562d6f1ea1b22e91500a5cc
Elliot <elliot.schrock@gmail.com>
2017-01-27 11:28:28 -0500

    fix response hash typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
 
   def response_hash
     {
-      twitter_handle: user.twitter_handle
+      twitter_handle: user.twitter_handle,
       api_key: user.api_key
     }    
   end
