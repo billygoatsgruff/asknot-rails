269b818152b1608d0413ad4db631b661fae2f4f1
Elliot <elliot.schrock@gmail.com>
2017-02-23 11:35:07 -0500

    add trends to admin

---
+++ app/admin/trend.rb
@@ -0,0 +1,29 @@
+ActiveAdmin.register Trend do
+
+  permit_params :twitter_handle
+
+  controller do
+    def create
+      @trend = Trend.new(permitted_params[:trend])
+
+      if @trend.save
+        redirect_to collection_url
+      else
+        redirect_to :back
+      end
+    end
+  end
+
+  form do |f|
+    f.inputs "Trend Info" do
+      f.input :text
+    end
+    f.actions
+  end
+
+  index do
+    column :id
+    column :text
+    column :created_at
+  end
+end
